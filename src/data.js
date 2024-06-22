56cf03c56609c8107987138c881a041c2821b938
bharadwaj211194 <vsnikhilbharadwajv@gmail.com>
2024-06-22 19:04:38 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Braclet for sagar",
+    title: "Ring for Nikhil",
     desc: "Barclets sale at 50% offer",
     bg: "green",
   },
