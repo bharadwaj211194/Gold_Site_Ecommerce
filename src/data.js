039bbcb1d515757c82a23b126863fc723df6fd2a
bharadwaj211194 <vsnikhilbharadwajv@gmail.com>
2025-01-10 11:29:24 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Ring for love",
+    title: "Ring for love life",
     desc: "Barclets sale at 50% offer",
     bg: "green",
   },
